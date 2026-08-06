8b78fa379eaaa0d39ee20bbc4d3cae549d9f5a51
Mao.Dev <114309283+MaoMachado@users.noreply.github.com>
2026-08-06 11:12:19 -0500

    feat: se esta creardo auth para crear eh iniciar con email y password

---
+++ apps/backend/src/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from './prisma/prisma.service';
+import bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async register(email: string, password: string) {
+    if (!email || !password) {
+      throw new Error('Email y contraseña son obligatorios');
+    }
+
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await this.prisma.usuario.create({
+        data: {
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      const { password: _, ...result } = user;
+      return result;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async login(email: string, password: string) {
+    if (!email || !password) {
+      throw new BadRequestException('Email y contraseña son obligatorios');
+    }
+
+    const user = await this.prisma.usuario.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('Usuario no encontrado');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new Error('Contraseña incorrecta');
+    }
+
+    const token = this.jwtService.sign({
+      id: user.id,
+      email: user.email,
+    });
+
+    return {
+      token,
+    };
+  }
+}
